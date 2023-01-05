c463800218512149b80faa01ed063fb9834a31d8
YimmyC <jim.cassano1@gmail.com>
2023-01-05 12:53:20 -0600

    good start, classes work

--- script/script.js
+++ script/script.js
@@ -1,7 +1,115 @@
+const today = dayjs();
+$("#currentDay").text(today.format("MMM D, YYYY"));
+
+const currentHour = dayjs().hour();
+let hour9El = $("#hour-9");
+let hour10El = $("#hour-10");
+let hour11El = $("#hour-11");
+let hour12El = $("#hour-12");
+let hour1El = $("#hour-1");
+let hour2El = $("#hour-2");
+let hour3El = $("#hour-3");
+let hour4El = $("#hour-4");
+let hour5El = $("#hour-5");
+console.log(currentHour);
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// Time of day class function
 $(function () {
+  if (currentHour === 9) {
+    console.log("works");
+    hour9El.addClass("present");
+    hour10El.addClass("future");
+    hour11El.addClass("future");
+    hour12El.addClass("future");
+    hour1El.addClass("future");
+    hour2El.addClass("future");
+    hour3El.addClass("future");
+    hour4El.addClass("future");
+    hour5El.addClass("future");
+  } else if (currentHour === 10) {
+    hour9El.addClass("past");
+    hour10El.addClass("present");
+    hour11El.addClass("future");
+    hour12El.addClass("future");
+    hour1El.addClass("future");
+    hour2El.addClass("future");
+    hour3El.addClass("future");
+    hour4El.addClass("future");
+    hour5El.addClass("future");
+  } else if (currentHour === 11) {
+    hour9El.addClass("past");
+    hour10El.addClass("past");
+    hour11El.addClass("present");
+    hour12El.addClass("future");
+    hour1El.addClass("future");
+    hour2El.addClass("future");
+    hour3El.addClass("future");
+    hour4El.addClass("future");
+    hour5El.addClass("future");
+  } else if (currentHour === 12) {
+    hour9El.addClass("past");
+    hour10El.addClass("past");
+    hour11El.addClass("past");
+    hour12El.addClass("present");
+    hour1El.addClass("future");
+    hour2El.addClass("future");
+    hour3El.addClass("future");
+    hour4El.addClass("future");
+    hour5El.addClass("future");
+  } else if (currentHour === 1) {
+    hour9El.addClass("past");
+    hour10El.addClass("past");
+    hour11El.addClass("past");
+    hour12El.addClass("past");
+    hour1El.addClass("present");
+    hour2El.addClass("future");
+    hour3El.addClass("future");
+    hour4El.addClass("future");
+    hour5El.addClass("future");
+  } else if (currentHour === 2) {
+    hour9El.addClass("past");
+    hour10El.addClass("past");
+    hour11El.addClass("past");
+    hour12El.addClass("past");
+    hour1El.addClass("past");
+    hour2El.addClass("present");
+    hour3El.addClass("future");
+    hour4El.addClass("future");
+    hour5El.addClass("future");
+  } else if (currentHour === 3) {
+    hour9El.addClass("past");
+    hour10El.addClass("past");
+    hour11El.addClass("past");
+    hour12El.addClass("past");
+    hour1El.addClass("past");
+    hour2El.addClass("past");
+    hour3El.addClass("present");
+    hour4El.addClass("future");
+    hour5El.addClass("future");
+  } else if (currentHour === 4) {
+    hour9El.addClass("past");
+    hour10El.addClass("past");
+    hour11El.addClass("past");
+    hour12El.addClass("past");
+    hour1El.addClass("past");
+    hour2El.addClass("past");
+    hour3El.addClass("past");
+    hour4El.addClass("present");
+    hour5El.addClass("future");
+  } else if (currentHour === 5) {
+    hour9El.addClass("past");
+    hour10El.addClass("past");
+    hour11El.addClass("past");
+    hour12El.addClass("past");
+    hour1El.addClass("past");
+    hour2El.addClass("past");
+    hour3El.addClass("past");
+    hour4El.addClass("past");
+    hour5El.addClass("present");
+  }
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,15 +117,8 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
 });
