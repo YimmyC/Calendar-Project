ec1b2860bf8f198e08d0c758166546e8a9ba7d17
YimmyC <jim.cassano1@gmail.com>
2023-01-09 13:21:11 -0600

    9-12 working

--- script/script.js
+++ script/script.js
@@ -21,38 +21,140 @@ let saveBtn3 = $("#3save");
 let saveBtn4 = $("#4save");
 let saveBtn5 = $("#5save");
 let textField9 = $("#9field");
+let textField10 = $("#10field");
+let textField11 = $("#11field");
+let textField12 = $("#12field");
+let textField1 = $("#1field");
+let textField2 = $("#2field");
+let textField3 = $("#3field");
+let textField4 = $("#4field");
+let textField5 = $("#5field");
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-function readTasksFromStorage() {
-  var tasks = localStorage.getItem("tasks");
-  if (tasks) {
-    tasks = JSON.parse(tasks);
+function init() {
+  printTask9Data();
+  printTask10Data();
+  printTask11Data();
+  printTask12Data();
+}
+// reads tasks
+function readTasks9FromStorage() {
+  var tasks9 = localStorage.getItem("tasks9");
+  if (tasks9) {
+    tasks9 = JSON.parse(tasks9);
   } else {
-    tasks = [];
+    tasks9 = [];
   }
   console.log("reading");
 
-  return tasks;
+  return tasks9;
 }
 
-function saveTasksToStorage(tasks) {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+function readTasks10FromStorage() {
+  var tasks10 = localStorage.getItem("tasks10");
+  if (tasks10) {
+    tasks10 = JSON.parse(tasks10);
+  } else {
+    tasks10 = [];
+  }
+  console.log("reading");
+
+  return tasks10;
 }
 
-function printTaskData() {
+function readTasks11FromStorage() {
+  var tasks11 = localStorage.getItem("tasks11");
+  if (tasks11) {
+    tasks11 = JSON.parse(tasks11);
+  } else {
+    tasks11 = [];
+  }
+  console.log("reading");
+
+  return tasks11;
+}
+
+function readTasks12FromStorage() {
+  var tasks12 = localStorage.getItem("tasks12");
+  if (tasks12) {
+    tasks12 = JSON.parse(tasks12);
+  } else {
+    tasks12 = [];
+  }
+  console.log("reading");
+
+  return tasks12;
+}
+// saves tasks
+function saveTasks9ToStorage(tasks9) {
+  localStorage.setItem("tasks9", JSON.stringify(tasks9));
+}
+
+function saveTasks10ToStorage(tasks10) {
+  localStorage.setItem("tasks10", JSON.stringify(tasks10));
+}
+
+function saveTasks11ToStorage(tasks11) {
+  localStorage.setItem("tasks11", JSON.stringify(tasks11));
+}
+
+function saveTasks12ToStorage(tasks12) {
+  localStorage.setItem("tasks12", JSON.stringify(tasks12));
+}
+// displays tasks
+function printTask9Data() {
   // get projects from localStorage
-  var tasks = readTasksFromStorage();
+  var tasks9 = readTasks9FromStorage();
 
-  console.log(tasks);
-  tasks.sort(function (a) {
+  console.log(tasks9);
+  tasks9.sort(function (a) {
     return a.task;
   });
 
   // $("#hour-9").append("<p>" + JSON.stringify(tasks) + "</p>");
-  document.getElementById("9field").value += JSON.stringify(tasks);
+  document.getElementById("9field").value += JSON.stringify(tasks9);
+}
+
+function printTask10Data() {
+  // get projects from localStorage
+  var tasks10 = readTasks10FromStorage();
+
+  console.log(tasks10);
+  tasks10.sort(function (a) {
+    return a.task;
+  });
+
+  // $("#hour-10").append("<p>" + JSON.stringify(tasks) + "</p>");
+  document.getElementById("10field").value += JSON.stringify(tasks10);
+}
+
+function printTask11Data() {
+  // get projects from localStorage
+  var tasks11 = readTasks11FromStorage();
+
+  console.log(tasks11);
+  tasks11.sort(function (a) {
+    return a.task;
+  });
+
+  // $("#hour-11").append("<p>" + JSON.stringify(tasks) + "</p>");
+  document.getElementById("11field").value += JSON.stringify(tasks11);
+}
+
+function printTask12Data() {
+  // get projects from localStorage
+  var tasks12 = readTasks12FromStorage();
+
+  console.log(tasks12);
+  tasks12.sort(function (a) {
+    return a.task;
+  });
+
+  // $("#hour-12").append("<p>" + JSON.stringify(tasks) + "</p>");
+  document.getElementById("12field").value += JSON.stringify(tasks12);
 }
 
 // Time of day class function
@@ -157,6 +259,8 @@ $(function () {
   //
 });
 
+init();
+
 saveBtn9.on("click", function (event) {
   event.preventDefault();
 
@@ -166,21 +270,60 @@ saveBtn9.on("click", function (event) {
   var newTask = { Task: taskInput };
 
   // add project to local storage
-  var tasks = readTasksFromStorage();
+  var tasks = readTasks9FromStorage();
   tasks.push(newTask);
-  saveTasksToStorage(tasks);
+  saveTasks9ToStorage(tasks);
 
   // print project data
-  printTaskData();
+  printTask9Data();
 });
-saveBtn10.on("click", function () {
-  console.log("i clicked");
+saveBtn10.on("click", function (event) {
+  event.preventDefault();
+
+  // read user input from the form
+  var taskInput = textField10.val().trim();
+
+  var newTask = { Task: taskInput };
+
+  // add project to local storage
+  var tasks = readTasks10FromStorage();
+  tasks.push(newTask);
+  saveTasks10ToStorage(tasks);
+
+  // print project data
+  printTask10Data();
 });
-saveBtn11.on("click", function () {
-  console.log("i clicked");
+saveBtn11.on("click", function (event) {
+  event.preventDefault();
+
+  // read user input from the form
+  var taskInput = textField11.val().trim();
+
+  var newTask = { Task: taskInput };
+
+  // add project to local storage
+  var tasks = readTasks11FromStorage();
+  tasks.push(newTask);
+  saveTasks11ToStorage(tasks);
+
+  // print project data
+  printTask11Data();
 });
-saveBtn12.on("click", function () {
-  console.log("i clicked");
+saveBtn12.on("click", function (event) {
+  event.preventDefault();
+
+  // read user input from the form
+  var taskInput = textField12.val().trim();
+
+  var newTask = { Task: taskInput };
+
+  // add project to local storage
+  var tasks = readTasks12FromStorage();
+  tasks.push(newTask);
+  saveTasks12ToStorage(tasks);
+
+  // print project data
+  printTask12Data();
 });
 saveBtn1.on("click", function () {
   console.log("i clicked");
